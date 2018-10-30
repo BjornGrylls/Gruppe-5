71f4bafad241c4e07b99b26e999fb2f5ff669119
BjornGrylls <bjornskole@gmail.com>
2018-10-30 08:52:24 +0100

    Userinput now working
    Unsure if .gitignore works

--- Gruppe 5/Gruppe 5/Program.cs
+++ Gruppe 5/Gruppe 5/Program.cs
@@ -9,6 +9,13 @@ namespace Gruppe_5 {
     class Program {
         static void Main(string[] args) {
 
+            // Initialize things
+            Program MyFunctions = new Program();
+            int heightOfPLayArea = 17;
+            
+
+
+
             Console.WriteLine("Press ESC to stop");
             do {
                 while (!Console.KeyAvailable) { // isRunning
@@ -23,13 +30,18 @@ namespace Gruppe_5 {
                         movingGroundArray[i] = '1';
                         if (i < movingGround.Length-1) { movingGroundArray[i + 1] = '_'; }
                         movingGround = new string(movingGroundArray);
-                        for (int j = 0; j < 17; j+=1) { // Move ground down
+
+                        for (int j = 0; j < heightOfPLayArea; j+=1) { // Move ground down
                             Console.WriteLine(" ");
                         }
                         Console.WriteLine(movingGround);
+
+                        while ((Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)) {
+                            Console.WriteLine("Jumping");
+                        }
+
                         Thread.Sleep(50);
                         Console.Clear();
-                        
                     }
                     movingGround = "______________________________________________________________________________________________________";
 
@@ -38,6 +50,11 @@ namespace Gruppe_5 {
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape); // until esc is pressed. Only works when the 1 is not on the line
         }
 
+        public void JumpTester() {
+            do {
+                Console.WriteLine("Jumping");
+            } while (Console.ReadKey(true).Key == ConsoleKey.Spacebar);
+        }
         
     }
 }
