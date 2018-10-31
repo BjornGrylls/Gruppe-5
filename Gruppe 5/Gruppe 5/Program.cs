d412acf9ae073d6b79eb45e494ca6404e0113384
Marcus Nielsen <nielsenmarcus24@gmail.com>
2018-10-31 08:05:45 +0100

    temp cactus

--- Gruppe 5/Gruppe 5/Program.cs
+++ Gruppe 5/Gruppe 5/Program.cs
@@ -5,24 +5,33 @@ using System.Text;
 using System.Threading;
 
 
-namespace Gruppe_5 {
-    class Program {
-        static void Main(string[] args) {
-
+namespace Gruppe_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
             Console.WriteLine("Press ESC to stop");
             do {
                 while (!Console.KeyAvailable) { // isRunning
 
-
-
+                   
+                    
                     string movingGround = "______________________________________________________________________________________________________";
 
-
                     for (int i = movingGround.Length-1; i >= 0; i -= 1) {
-                        char[] movingGroundArray = movingGround.ToCharArray();
-                        movingGroundArray[i] = '1';
-                        if (i < movingGround.Length-1) { movingGroundArray[i + 1] = '_'; }
-                        movingGround = new string(movingGroundArray);
+                        string[] movingGroundArray = movingGround.ToStringArray();
+                        movingGroundArray[i] = "  ___ \n" +
+                                               "   | |\n" +
+                                               "   | | \n" +
+                                               "|| | | ||\n" +
+                                               "||_| |_||\n" +
+                                               "   | |\n" +
+                                               "   | |\n";
+                        if (i < movingGround.Length-1) { movingGroundArray[i + 1] = "_"; }
+
                         for (int j = 0; j < 17; j+=1) { // Move ground down
                             Console.WriteLine(" ");
                         }
@@ -37,7 +46,19 @@ namespace Gruppe_5 {
                 }
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape); // until esc is pressed. Only works when the 1 is not on the line
         }
-
+        
+        public void PrintCactus() //the cactus
+        {
+           Console.WriteLine("   ___ \n" +
+                "   | |\n" +
+                "  | | \n" +
+                "|| | | ||\n" +
+                "||_| |_||\n" +
+                "   | |\n" +
+                "   | |\n");
+        }
+       
         
     }
 }
+
